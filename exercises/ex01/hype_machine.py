0b9c1f5b3aada92645c639ad9dee977884b07473
Saira Malik <sairas@email.unc.edu>
2021-08-31 18:21:25 -0400

    “Progress on Exercise 1”

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,11 +1,11 @@
 """Hype machine exercise to boost your spirit"""
 
-_author_ = " 730439074 "
+_author_ = "730439074"
 
 name: str = input("What is your name? ")
 
 
-print( "You got this " + str(name) + " keep trying! ")
+print( "You got this, " + str(name) + " keep trying! ")
 
 print(str(name) + " is amazing ")
 
