f2dd33a05c7dafac2933ec46a753c37e5e147b47
Saira Malik <sairas@email.unc.edu>
2021-09-15 16:19:33 -0400

    Progress on Exercise 2

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730439074"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,24 @@ from random import randint
 
 
 # Begin your solution here...
+print("Pick a number between 1-4")
+fortune_num = (randint(1, 4))
+
+
+fortune_1 = "I am worth a fortune"
+fortune_2 = "The fortune you seek is in another cookie"
+fortune_3 = "An alien of some sort will be appearing to you shortly"
+fortune_4 = "You can always find happiness at work on Friday"
+
+if (fortune_num == 1): 
+    print("your fortune cookie says..." + fortune_1)
+else: 
+    print(fortune_2)
+
+if (fortune_num == 3):
+    print(fortune_3)
+else:
+    print(fortune_4)
+
+
+
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,16 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730439074""
 
 
 # Begin your solution here...
+user_string: str = input("What beat do you want to repeat?")
+
+
+
+
+counter: int = 0 
+while counter <= 0 
+    print("What beat do you eant to repeat? boom")
+    print('How many times do want to repeat it'+ counter )
+    print( "No beat...")
