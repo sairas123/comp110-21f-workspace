ff4725aabe3b325b35db1b4f5e60098e070b5456
Saira Malik <sairas@email.unc.edu>
2021-09-29 18:34:27 -0400

    progrss on Exercise 3

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,13 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730439074"
 
 
 # TODO: Implement your functions here.
+from random import randint
+
+def all(x:list[int], y: int) -> bool:
+    while True:
+        x == y 
+        if x != y:
+            False
