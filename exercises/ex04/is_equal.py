7456b36fa65658a854a14635e53620f28921627f
Saira Malik <sairas@email.unc.edu>
2021-09-29 19:20:41 -0400

    progress on exercise 3

--- exercises/ex04/is_equal.py
+++ exercises/ex04/is_equal.py
@@ -1,9 +1,11 @@
-"""Exercise is equal ."""
+"""Exercise on deep equality and comparing two lists."""
+
+
 __author__ = "730439074"
+
 def is_equal(x:list[int], y: list[int]) -> bool:  
 
-
-    if x == y:
+    if x == y: 
         print(True)
     else:
         print(False)
--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,14 +1,26 @@
 """List utility functions."""
-
 __author__ = "730439074"
-
 
 # TODO: Implement your functions here.
 
+"""Exercise all to practice built-in len function."""
 
-def all(x:list[int], y: int) -> bool: 
 
+def all(x:list[int], y: int) -> bool:  
 
     if len(x) == y:
         return True
     return False
+
+
+"""Exercise on deep equality and comparing two lists."""
+
+
+__author__ = "730439074"
+
+def is_equal(x:list[int], y: list[int]) -> bool:  
+
+    if x == y: 
+        print(True)
+    else:
+        print(False)
