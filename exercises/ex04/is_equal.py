f20e9bf9beba60834ba50e79fdd33e7da153c657
Saira Malik <sairas@email.unc.edu>
2021-09-29 19:48:55 -0400

    progress on exercise 3

--- exercises/ex04/is_equal.py
+++ exercises/ex04/is_equal.py
@@ -3,9 +3,9 @@
 
 __author__ = "730439074"
 
-def is_equal(x:list[int], y: list[int]) -> bool:  
+def is_equal(a:list[int], b: list[int]) -> bool:  
 
-    if x == y: 
+    while a == b: 
         print(True)
     else:
         print(False)
