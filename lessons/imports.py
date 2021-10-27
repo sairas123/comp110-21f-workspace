a42cefd036f8837c9bc6c30653561f95ca596751
Saira Malik <sairas@email.unc.edu>
2021-10-27 19:49:34 -0400

    Progress on lessons

---
+++ lessons/imports.py
@@ -0,0 +1,21 @@
+"""Examples of importing python."""
+
+
+from lessons import helpers
+
+#Alias a module/ imported name as another name
+from lessons import helpers as hp
+
+# Import names defined globally in a module
+from lessons.helpers import powerful
+
+
+def main() -> None:
+    """Entrypoint of program."""
+    print(helpers.powerful(2, 4))
+    print(f"The answer: {hp.THE_ANSWER}")
+    print(powerful(2, 4))
+
+
+    if __name__ ==  "__main__":
+        main()
